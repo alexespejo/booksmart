2aed135a1163faeed42a8df3a8a3d583eea4b2a4
Alex Espejo <aespejo102@gmail.com>
2023-12-13 14:34:31 -0800

    created view customer api

--- pages/viewOrders.vue
+++ pages/viewOrders.vue
@@ -1,18 +1,11 @@
 <script setup>
-async function updateBooks() {
- await $fetch("/api/addBookToAuthor", {
-  method: "post",
-  body: {
-   authorID: "107",
-   bookID: "DICKENSON8596",
-  },
- });
-}
+const { pending, data: orders } = await useLazyFetch("/api/getOrders");
 </script>
 <template>
  <NuxtLayout>
-  <main class="flex items-end justify-end">
-   <button class="btn" @click="updateBooks">click</button>
+  <DataLoading v-if="pending" />
+  <main v-else class="flex items-end justify-end">
+   {{ orders.orders }}
   </main>
  </NuxtLayout>
 </template>
